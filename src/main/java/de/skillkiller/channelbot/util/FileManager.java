c8fbef30584c90512d65b5917e2963e0c800e21f
ゼクロ <zekrommaster110@aol.de>
2017-10-25 21:54:30 +0200

    fixed an import :^)

--- src/main/java/de/skillkiller/channelbot/util/FileManager.java
+++ src/main/java/de/skillkiller/channelbot/util/FileManager.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 /**
  * Created by Skillkiller on 25.06.2017.
